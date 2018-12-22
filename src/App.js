c36615e9f77214f8975226ebd399819eba31a9ae
Andrés Osante <aosantea@ucenfote.ac.cr>
2018-12-21 21:42:10 -0600

    .

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
 
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1 />
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <div className="container">
+            <li />
+          </div>
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class Navbar extends Component {
+  render() {
+    return (
+      <nav>
+        <div className="container">
+          <Link to="/" className="link">
+            Client Panel
+          </Link>
+          <button
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarMain"
+          />
+          <div id="#navbarMain">
+            <ul>
+              <li>
+                <Link to="/">Dashboard</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
